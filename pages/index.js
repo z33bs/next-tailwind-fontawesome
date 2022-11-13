2c9ce4bde22db59c1360a4a2ea9cbc80c5eeda03
z33bs <zeebsmail@gmail.com>
2022-11-13 17:40:05 +0000

    Install TailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-          Hello World!
+          <h1 className="text-3xl font-bold underline">
+              Testing tailwind
+          </h1>
       </main>
     </div>
   )
